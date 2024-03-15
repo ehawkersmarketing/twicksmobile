5bbd9b015d4e0bd36258343d3fff05a25590fc79
Anmol <tech@ehawkersmarketing.in>
2024-03-15 12:47:13 +0530

    updates

--- twicksmobile/app.config.js
+++ twicksmobile/app.config.js
@@ -1,7 +1,18 @@
-export default {
-    name: 'twicksmobile',
-    slug: 'twicksmobile',
-    scheme: 'twicks', 
+// const myValue = 'Twicks'
+// module.exports = {
+    
+//     name: myValue,
+//     version: process.env.PROJECT_VERSION,
+//     slug: 'twicksmobile',
+//     scheme: 'twicks', 
+//     expo: {
+//         "extra": {
+//           "eas": {
+//             "projectId": "4a499923-de48-41ed-8c8e-cb76fd191323"
+//           }
+//         }
+//       }
 
-   };
+//    };
+   
    
